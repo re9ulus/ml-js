45492ecc2eeb778f51295f83f84680b36feb8495
Vasiliy Zemlyanov <angeldaren@gmail.com>
2017-05-10 10:17:43 +0300

    Update metrics.test, add accuracy tests

--- src/ml/__tests__/metrics.test.js
+++ src/ml/__tests__/metrics.test.js
@@ -17,3 +17,23 @@ test('MSE equal', () => {
 test('MSE diff', () => {
   expect(Mtr.meanSquaredError([1, 2, 3], [4, 5, 6])).toEqual(9);
 });
+
+
+test('Accuracy different length', () => {
+  expect(() => {
+    Mtr.accuracy([1], [2, 3])}
+  ).toThrow();
+});
+
+test('Accuracy empty', () => {
+  expect(Mtr.accuracy([], [])).toEqual(1);
+});
+
+test('Accuracy equal', () => {
+  expect(Mtr.accuracy([1, 0, 1], [1, 0, 1])).toEqual(1);
+});
+
+test('Accuracy diff', () => {
+  expect(Mtr.accuracy([1, 1, 1], [0, 0, 0])).toEqual(0);
+  expect(Mtr.accuracy([1, 1, 0, 0], [1, 0, 1, 0])).toEqual(0.5);
+});
